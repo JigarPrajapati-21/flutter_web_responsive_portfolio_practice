9ccc7efadf2c3de48c62bf79749bf928076a937c
JigarPrajapati-21 <jigarprajapati9950@gmail.com>
2025-09-01 15:16:18 +0530

    commit

--- lib/aboutme_section/aboutme_section.dart
+++ lib/aboutme_section/aboutme_section.dart
@@ -32,60 +32,116 @@ class AboutmeSectionDesktop extends StatelessWidget {
         children: [
           SizedBox(height: 20,),
 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                constraints: BoxConstraints(
-                  // maxHeight: 525,
-                  minWidth: MediaQuery.sizeOf(context).width/3,
-                  minHeight: MediaQuery.sizeOf(context).height/2,
-                  // maxWidth: 525
-                ),
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage("assets/3.png"))
-                ),
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.symmetric(horizontal: MediaQuery.sizeOf(context).width / 20),
+
+            width: double.infinity,
+            // margin: EdgeInsets.symmetric(vertical: 60),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 400,
+                    width: 400,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/3.png"),
+                      ),
+                    ),
+                  ),
+
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+
+
+                        Text("About Me",style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black
+                        ),),
+
+                        SizedBox(height: 30,),
+
+
+                        Text(                  textAlign: TextAlign.justify,
+                            "I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js). I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience, pixel perfect design, and writing clear, readable, highly performant code matters to me."),
+                        SizedBox(height: 10,),
+                        Text(                  textAlign: TextAlign.justify,
+                            "I began my journey as a web developer in 2015, and since then, I've continued to grow and evolve as a developer, taking on new challenges and learning the latest technologies along the way. Now, in my early thirties, 7 years after starting my web development journey, I'm building cutting-edge web applications using modern technologies such as Next.js, TypeScript, Nestjs, Tailwindcss, Supabase and much more."),
+                        SizedBox(height: 10,),
+                        Text(                  textAlign: TextAlign.justify,
+                            "When I'm not in full-on developer mode, you can find me hovering around on twitter or on indie hacker, witnessing the journey of early startups or enjoying some free time. You can follow me on Twitter where I share tech-related bites and build in public, or you can follow me on GitHub."),
+
+
+                      ],
+                    ),
+                  ),
+                ],
               ),
-
-              SizedBox(width: 20,),
-
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text("About Me",style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black
-                    ),),
-
-SizedBox(height: 30,),
-
-
-                    Text(                  textAlign: TextAlign.justify,
-                        "I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js). I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience, pixel perfect design, and writing clear, readable, highly performant code matters to me."),
-SizedBox(height: 10,),
-                    Text(                  textAlign: TextAlign.justify,
-                        "I began my journey as a web developer in 2015, and since then, I've continued to grow and evolve as a developer, taking on new challenges and learning the latest technologies along the way. Now, in my early thirties, 7 years after starting my web development journey, I'm building cutting-edge web applications using modern technologies such as Next.js, TypeScript, Nestjs, Tailwindcss, Supabase and much more."),
-SizedBox(height: 10,),
-                    Text(                  textAlign: TextAlign.justify,
-                        "When I'm not in full-on developer mode, you can find me hovering around on twitter or on indie hacker, witnessing the journey of early startups or enjoying some free time. You can follow me on Twitter where I share tech-related bites and build in public, or you can follow me on GitHub."),
-
-                  ],
-                ),
-              ),
-
-
-
-            ],
+            ),
           ),
+
+
+
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             children: [
+//               Container(
+//                 constraints: BoxConstraints(
+//                   // maxHeight: 525,
+//                   minWidth: MediaQuery.sizeOf(context).width/3,
+//                   minHeight: MediaQuery.sizeOf(context).height/2,
+//                   // maxWidth: 525
+//                 ),
+//                 decoration: BoxDecoration(
+//                   image: DecorationImage(image: AssetImage("assets/3.png"))
+//                 ),
+//               ),
+//
+//               SizedBox(width: 20,),
+//
+//               Expanded(
+//                 child: Column(
+//                   mainAxisSize: MainAxisSize.min,
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   crossAxisAlignment: CrossAxisAlignment.center,
+//                   children: [
+//                     Text("About Me",style: TextStyle(
+//                         fontSize: 35,
+//                         fontWeight: FontWeight.bold,
+//                         color: Colors.black
+//                     ),),
+//
+// SizedBox(height: 30,),
+//
+//
+//                     Text(                  textAlign: TextAlign.justify,
+//                         "I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js). I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience, pixel perfect design, and writing clear, readable, highly performant code matters to me."),
+// SizedBox(height: 10,),
+//                     Text(                  textAlign: TextAlign.justify,
+//                         "I began my journey as a web developer in 2015, and since then, I've continued to grow and evolve as a developer, taking on new challenges and learning the latest technologies along the way. Now, in my early thirties, 7 years after starting my web development journey, I'm building cutting-edge web applications using modern technologies such as Next.js, TypeScript, Nestjs, Tailwindcss, Supabase and much more."),
+// SizedBox(height: 10,),
+//                     Text(                  textAlign: TextAlign.justify,
+//                         "When I'm not in full-on developer mode, you can find me hovering around on twitter or on indie hacker, witnessing the journey of early startups or enjoying some free time. You can follow me on Twitter where I share tech-related bites and build in public, or you can follow me on GitHub."),
+//
+//                   ],
+//                 ),
+//               ),
+//
+//
+//
+//             ],
+//           ),
           
           
 
 
-          SizedBox(height: 40,),
+          // SizedBox(height: 10,),
 
 
         ],
