f72c52dcf4fb88e7433d1dfcb27a6ba85ec6d1e0
JigarPrajapati-21 <jigarprajapati9950@gmail.com>
2025-09-01 14:56:18 +0530

    commit

--- lib/project_section/project_section.dart
+++ lib/project_section/project_section.dart
@@ -646,6 +646,10 @@ class ProjectSectionDesktop extends StatelessWidget {
           const SizedBox(height: 50),
 
 
+
+
+          // First Project
+
           Container(
             color: Colors.yellowAccent,
             padding: EdgeInsets.symmetric(horizontal: screenWidth / 20),
@@ -706,85 +710,97 @@ class ProjectSectionDesktop extends StatelessWidget {
           ),
 
 
-          // First Project
-          Container(
-            color: Colors.blueAccent,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: screenHeight/2,
-                  width: 200,//screenWidth /3,
-                  // height: screenHeight /3,
-                  // width: screenWidth /2,
-                  margin: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                    // color: Colors.greenAccent,
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-
-                    image: DecorationImage(
-                      image: AssetImage("assets/4.png"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 30),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          "01",
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          "Crypto Screener Application",
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          "The Crypto Screener Application is a powerful tool designed to help users track, analyze, and filter cryptocurrencies in real time based on various parameters such as price, market capitalization, trading volume, percentage change, and more. The application provides an intuitive and responsive interface where users can search and sort coins, view detailed charts, and monitor live market trends. It allows investors and traders to customize filters and watchlists according to their preferences, making it easier to identify top-performing coins or spot potential investment opportunities. With real-time updates and an easy-to-use dashboard, the Crypto Screener Application simplifies crypto market analysis for beginners as well as experienced traders.",
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        SizedBox(height: 20),
-                        Icon(Icons.open_in_new,
-                            color: Colors.white, size: 22),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          // Container(
+          //   color: Colors.blueAccent,
+          //   child: Row(
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     children: [
+          //       Container(
+          //         height: screenHeight/2,
+          //         width: 200,//screenWidth /3,
+          //         // height: screenHeight /3,
+          //         // width: screenWidth /2,
+          //         margin: const EdgeInsets.all(10),
+          //         decoration: const BoxDecoration(
+          //           // color: Colors.greenAccent,
+          //           borderRadius: BorderRadius.all(Radius.circular(20)),
+          //
+          //           image: DecorationImage(
+          //             image: AssetImage("assets/4.png"),
+          //             fit: BoxFit.cover,
+          //           ),
+          //         ),
+          //       ),
+          //       Expanded(
+          //         child: Padding(
+          //           padding: const EdgeInsets.symmetric(
+          //               vertical: 10, horizontal: 30),
+          //           child: Column(
+          //             crossAxisAlignment: CrossAxisAlignment.start,
+          //             children: const [
+          //               Text(
+          //                 "01",
+          //                 style: TextStyle(
+          //                   fontSize: 30,
+          //                   fontWeight: FontWeight.bold,
+          //                   color: Colors.white,
+          //                 ),
+          //               ),
+          //               SizedBox(height: 20),
+          //               Text(
+          //                 "Crypto Screener Application",
+          //                 style: TextStyle(
+          //                   fontSize: 25,
+          //                   fontWeight: FontWeight.bold,
+          //                   color: Colors.white,
+          //                 ),
+          //               ),
+          //               SizedBox(height: 20),
+          //               Text(
+          //                 "The Crypto Screener Application is a powerful tool designed to help users track, analyze, and filter cryptocurrencies in real time based on various parameters such as price, market capitalization, trading volume, percentage change, and more. The application provides an intuitive and responsive interface where users can search and sort coins, view detailed charts, and monitor live market trends. It allows investors and traders to customize filters and watchlists according to their preferences, making it easier to identify top-performing coins or spot potential investment opportunities. With real-time updates and an easy-to-use dashboard, the Crypto Screener Application simplifies crypto market analysis for beginners as well as experienced traders.",
+          //                 textAlign: TextAlign.justify,
+          //                 style: TextStyle(color: Colors.grey),
+          //               ),
+          //               SizedBox(height: 20),
+          //               Icon(Icons.open_in_new,
+          //                   color: Colors.white, size: 22),
+          //             ],
+          //           ),
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
 
           const SizedBox(height: 40),
 
           // Second Project
-          Container(
-            color: Colors.greenAccent,
 
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 30),
+          Container(
+            color: Colors.yellowAccent,
+            padding: EdgeInsets.symmetric(horizontal: screenWidth / 20),
+
+            width: double.infinity,
+            // margin: EdgeInsets.symmetric(vertical: 60),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 400,
+                    width: 400,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/6.png"),
+                      ),
+                    ),
+                  ),
+
+                  Flexible(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
+                      children: [
                         Text(
                           "02",
                           style: TextStyle(
@@ -814,50 +830,99 @@ class ProjectSectionDesktop extends StatelessWidget {
                       ],
                     ),
                   ),
-                ),
-                Container(
-                  height: screenHeight/2,
-                  width: screenWidth * 0.35,
-                  margin: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    image: DecorationImage(
-                      image: AssetImage("assets/6.png"),
-                      fit: BoxFit.cover,                  ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
+
+          //
+          // Container(
+          //   color: Colors.greenAccent,
+          //
+          //   child: Row(
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     children: [
+          //       Expanded(
+          //         child: Padding(
+          //           padding: const EdgeInsets.symmetric(
+          //               vertical: 10, horizontal: 30),
+          //           child: Column(
+          //             crossAxisAlignment: CrossAxisAlignment.start,
+          //             children: const [
+          //               Text(
+          //                 "02",
+          //                 style: TextStyle(
+          //                   fontSize: 30,
+          //                   fontWeight: FontWeight.bold,
+          //                   color: Colors.white,
+          //                 ),
+          //               ),
+          //               SizedBox(height: 20),
+          //               Text(
+          //                 "Euphoria - Ecommerce (Apparels) Website Template",
+          //                 style: TextStyle(
+          //                   fontSize: 25,
+          //                   fontWeight: FontWeight.bold,
+          //                   color: Colors.white,
+          //                 ),
+          //               ),
+          //               SizedBox(height: 20),
+          //               Text(
+          //                 "The Euphoria – Ecommerce (Apparels) Website Template is a modern, responsive, and user-friendly template designed specifically for online apparel stores. It offers a clean and stylish interface that highlights products with high-quality visuals and intuitive navigation, ensuring a seamless shopping experience across all devices. The template includes essential ecommerce features such as product listings, detailed product pages, shopping cart, wishlist, and checkout flow, along with categories and filters to make browsing effortless. Built with scalability and customization in mind, it allows store owners to easily tailor the design and content to match their brand identity, making it a perfect solution for launching a professional online fashion store.",
+          //                 textAlign: TextAlign.justify,
+          //                 style: TextStyle(color: Colors.grey),
+          //               ),
+          //               SizedBox(height: 20),
+          //               Icon(Icons.open_in_new,
+          //                   color: Colors.white, size: 22),
+          //             ],
+          //           ),
+          //         ),
+          //       ),
+          //       Container(
+          //         height: screenHeight/2,
+          //         width: screenWidth * 0.35,
+          //         margin: const EdgeInsets.all(10),
+          //         decoration: const BoxDecoration(
+          //           borderRadius: BorderRadius.all(Radius.circular(20)),
+          //           image: DecorationImage(
+          //             image: AssetImage("assets/6.png"),
+          //             fit: BoxFit.cover,                  ),
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
 
           const SizedBox(height: 40),
 
           // Third Project
+
           Container(
-            color: Colors.greenAccent,
+            color: Colors.yellowAccent,
+            padding: EdgeInsets.symmetric(horizontal: screenWidth / 20),
 
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: screenHeight/2,
-                  width: screenWidth * 0.35,
-                  margin: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-
-                    image: DecorationImage(
-                      image: AssetImage("assets/5.png"),
-                      fit: BoxFit.cover,                  ),
+            width: double.infinity,
+            // margin: EdgeInsets.symmetric(vertical: 60),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 400,
+                    width: 400,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/5.png"),
+                      ),
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 30),
+
+                  Flexible(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
+                      children: [
                         Text(
                           "03",
                           style: TextStyle(
@@ -887,10 +952,69 @@ class ProjectSectionDesktop extends StatelessWidget {
                       ],
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
+
+          // Container(
+          //   color: Colors.greenAccent,
+          //
+          //   child: Row(
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     children: [
+          //       Container(
+          //         height: screenHeight/2,
+          //         width: screenWidth * 0.35,
+          //         margin: const EdgeInsets.all(10),
+          //         decoration: const BoxDecoration(
+          //           borderRadius: BorderRadius.all(Radius.circular(20)),
+          //
+          //           image: DecorationImage(
+          //             image: AssetImage("assets/5.png"),
+          //             fit: BoxFit.cover,                  ),
+          //         ),
+          //       ),
+          //       Expanded(
+          //         child: Padding(
+          //           padding: const EdgeInsets.symmetric(
+          //               vertical: 10, horizontal: 30),
+          //           child: Column(
+          //             crossAxisAlignment: CrossAxisAlignment.start,
+          //             children: const [
+          //               Text(
+          //                 "03",
+          //                 style: TextStyle(
+          //                   fontSize: 30,
+          //                   fontWeight: FontWeight.bold,
+          //                   color: Colors.white,
+          //                 ),
+          //               ),
+          //               SizedBox(height: 20),
+          //               Text(
+          //                 "Blog Website Template",
+          //                 style: TextStyle(
+          //                   fontSize: 25,
+          //                   fontWeight: FontWeight.bold,
+          //                   color: Colors.white,
+          //                 ),
+          //               ),
+          //               SizedBox(height: 20),
+          //               Text(
+          //                 "A Blog Website Template is a ready-to-use, customizable web design layout created for individuals or organizations who want to share articles, stories, tutorials, or news online. This project provides a clean and responsive user interface that works seamlessly across devices, offering sections such as a homepage to showcase featured posts, category-based blog listings, individual blog detail pages, an about page, and a contact form. It focuses on readability, smooth navigation, and engaging design while maintaining flexibility for personalization. The template can be easily adapted for personal blogs, professional writing portfolios, or company news portals, making it a versatile starting point for building a modern blogging platform.",
+          //                 textAlign: TextAlign.justify,
+          //                 style: TextStyle(color: Colors.grey),
+          //               ),
+          //               SizedBox(height: 20),
+          //               Icon(Icons.open_in_new,
+          //                   color: Colors.white, size: 22),
+          //             ],
+          //           ),
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
 
           const SizedBox(height: 60),
         ],
